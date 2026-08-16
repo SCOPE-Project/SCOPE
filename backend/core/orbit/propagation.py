5f7469f2010bf7181d7ad95068f5443e94c745a8
Christopher Vogt <st173287@stud.uni-stuttgart.de>
2026-08-16 16:44:14 +0200

    Delete Deprecated Code from core

--- backend/core/orbit/propagation.py
+++
@@ -1,9 +0,0 @@
-from typing import Callable
-
-def propagate_orbit(selected_satellites: list, on_progress_update: Callable):
-    print("Propagate orbit started")
-    pass
-
-def extract_overpasses(selected_satellites: list, on_progress_update: Callable):
-    print("Extract overpasses started")
-    pass
